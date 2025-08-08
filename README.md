d3973a829fb2b758641daa88ec2dc2f3e2af2636
YarneD-1952226 <71635329+YarneD-1952226@users.noreply.github.com>
2025-08-08 12:16:05 +0200

    File updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# A-Multimodal-AI-Driven-GUI-Framework-for-Dynamic-User-Adaptation
-A Multimodal AI-Driven GUI Framework for Dynamic User Adaptation
--- backend/Interface/backend_interface/android/app/src/main/kotlin/com/example/backend_interface/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.backend_interface
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
